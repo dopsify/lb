928ca109117342d77f99f4f10dc12d85e8f5c3ee
Your Name <you@example.com>
2021-06-27 14:59:15 -0700

    support for CD job

--- dfy_scripts/build_and_push.sh
+++ dfy_scripts/build_and_push.sh
@@ -9,8 +9,5 @@ image_registry="quay.io/sohan_karun/dopsify"
 tag="${version}-${BUILD_NUMBER}"
 docker_image="${image_registry}_${app_name}:${tag}"
 
-
-echo "Building the docker image"
+echo "${docker_image}"
 docker build -t "${docker_image}" -f ./Dockerfile .
-echo "$QUAY_BOT_PASSWORD" | docker login -u "$QUAY_BOT_USERNAME" --password-stdin quay.io
-docker push ${docker_image}
